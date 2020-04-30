5bc47006c584d09296a9f2877120e3d0b6680923
adamszabozd <64538578+adamszabozd@users.noreply.github.com>
2020-04-30 19:30:52 +0200

    friss

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 console.log("Script file is loaded");
+$('h1').html("This is done by <em>JS!</em>");
+$('h2').toggleClass('blue');
+
+function myLittleFunction(){
+    $('main').append('<p>A p is appended in the main</p>');
+    $('p').css('border', '1px solid red');
+    $('p').css('10px', 'padding');
+}
+$('#add').click(myLittleFunction);
+$('#remove').click (function (){
+    $('p:last-child').remove();
+});
